d4e7a32fb37211fefd3be8760f413449272a92bb
Shivam-GitLab <shivam_223033@saitm.org>
2026-01-18 15:55:14 +0530

    config file added

---
+++ src/main/java/com/sm/smartEmail/SmartEmailApplication.java
@@ -0,0 +1,13 @@
+package com.sm.smartEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/smartEmail/SmartEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.smartEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
